83d515ffad10b741e3be9934a4f0d51d44d38702
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-11 15:51:47 -0300

    Changes cors port

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    'Content-type': 'application/json',
+    "Content-type": "application/json",
   },
 });
